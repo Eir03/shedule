16c58c6e509da0b2c6036cdc7e38fee189e963e7
Староверов Данила Андреевич <staroverovdanila@gmail.com>
2023-11-13 09:18:47 +0300

    fix

--- filter.py
+++ filter.py
@@ -15,13 +15,25 @@ def sortBy():
         if cell.value:
             coordinate.append(cell.coordinate)
             groups.append(cell.value)
+
     groups = [x.replace('группа', '').replace(' ', '') for x in groups]
     groups_dict = dict(zip(groups, coordinate))
     print(groups_dict)
+
     for group in groups_dict:
+        # Получаем ячейку по координатам и смещаем ее на 2 вниз и 2 вправо
         cell = sheet[groups_dict[group]]
-        cell = cell.offset(1, 0)
-        print(cell)
+        cell = sheet[cell.row + 3][cell.column - 1]
+        print(group)
+        for i in range(5):
+            number = cell.value
+            subject = sheet[cell.row][cell.column + 1].value
+            teach = sheet[cell.row + 1][cell.column + 1].value
+            cabinet = sheet[cell.row][cell.column + 4].value
+            print(number, subject.replace('\n', ' ') if subject is not None else '', teach, cabinet)
+            cell = sheet[cell.row + 2][cell.column - 1]
+        break
+
 
 
 
